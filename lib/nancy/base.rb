d24d7c118d4864fd322d11e648e489c4ce771ac9
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-01-04 17:34:04 -0500

    Refactor code: Set methods access level propertly

--- lib/nancy/base.rb
+++ lib/nancy/base.rb
@@ -2,8 +2,6 @@ require 'rack'
 
 module Nancy
   class Base
-    attr_reader :request, :response, :params, :env
-
     class << self
       %w(GET POST PATCH PUT DELETE HEAD OPTIONS).each do |verb|
         define_method(verb.downcase) do |pattern, &block|
@@ -16,77 +14,60 @@ module Nancy
           filters[filter.to_sym] = block
         end
       end
-    end
 
-    def self.compile(pattern)
-      keys = []
-      pattern.gsub!(/(:\w+)/) do |match|
-        keys << $1[1..-1]
-        "([^/?#]+)"
+      def use(*args, &block)
+        @builder.use(*args, &block)
       end
-      [%r{^#{pattern}$}, keys]
-    end
 
-    def self.route_set
-      @route_set ||= Hash.new { |hash, key| hash[key] = [] }
-    end
+      def map(*args, &block)
+        @builder.map(*args, &block)
+      end
 
-    def self.filters
-      @filters ||= Hash.new
-    end
+      def inherited(child)
+        child.instance_eval do
+          @builder = Rack::Builder.new
+          @builder.run(child.new)
+        end
+      end
 
-    def session
-      request.env["rack.session"] || raise("Rack::Session handler is missing")
-    end
+      def route_set
+        @route_set ||= Hash.new { |hash, key| hash[key] = [] }
+      end
 
-    def self.use(*args, &block)
-      @builder.use(*args, &block)
-    end
+      def filters
+        @filters ||= Hash.new
+      end
 
-    def self.map(*args, &block)
-      @builder.map(*args, &block)
-    end
+      def call(env)
+        @builder.dup.call(env)
+      end
 
-    def self.inherited(child)
-      child.instance_eval do
-        @builder = Rack::Builder.new
-        @builder.run(child.new)
+      private
+
+      def compile(pattern)
+        keys = []
+        pattern.gsub!(/(:\w+)/) do |match|
+          keys << $1[1..-1]
+          "([^/?#]+)"
+        end
+        [%r{^#{pattern}$}, keys]
       end
     end
 
-    def self.call(env)
-      @builder.dup.call(env)
-    end
+    attr_reader :request, :response, :params, :env
 
     def call(env)
+      env['PATH_INFO'] = '/' if env['PATH_INFO'].empty?
       @request = Rack::Request.new(env)
       @response = Rack::Response.new
       @params = request.params
       @env = env
-      catch(:halt) do
-        route_eval(request.request_method, request.path_info)
-      end.finish
+      route_eval
+      @response.finish
     end
 
-    def filters
-      self.class.filters
-    end
-
-    def route_eval(request_method, path_info)
-      path_info = "/" if path_info == ""
-      self.class.route_set[request_method].each do |matcher, block|
-        if match = path_info.match(matcher[0])
-          if (captures = match.captures) && !captures.empty?
-            url_params = Hash[*matcher[1].zip(captures).flatten]
-            @params = url_params.merge(params)
-          end
-          instance_exec(&filters[:before]) if filters[:before]
-          response.write instance_eval(&block)
-          instance_exec(&filters[:after]) if filters[:after]
-          return response
-        end
-      end
-      halt 404
+    def session
+      request.env["rack.session"] || raise("Rack::Session handler is missing")
     end
 
     def halt(*res)
@@ -94,6 +75,26 @@ module Nancy
       response.header.merge!(res.detect{|x| x.is_a?(Hash) } || {})
       response.body = [res.detect{|x| x.is_a?(String) } || ""]
       throw :halt, response
+    end
+
+    private
+
+    def route_eval
+      catch(:halt) do
+        self.class.route_set[@request.request_method].each do |matcher, block|
+          if match = @request.path_info.match(matcher[0])
+            if (captures = match.captures) && !captures.empty?
+              url_params = Hash[*matcher[1].zip(captures).flatten]
+              @params = url_params.merge(params)
+            end
+            instance_exec(&self.class.filters[:before]) if self.class.filters[:before]
+            @response.write instance_eval(&block)
+            instance_exec(&self.class.filters[:after]) if self.class.filters[:after]
+            return
+          end
+        end
+        halt 404
+      end
     end
   end
 end
